f89aa1571c5b1f87209eb3f3af92ca3e30834dbc
Sahilgill24 <gursahilgill24@gmail.com>
2023-03-13 17:27:50 +0530

    virtual mouse and eyecam working

--- alfred.py
+++ alfred.py
@@ -1,59 +1,51 @@
+import pyttsx3 as pt
+from mouse import videoon
+from eyecam import eyecamon
 import speech_recognition as sr
-import pyttsx3
-import pywhatkit
+import pyaudio
 import datetime
-import wikipedia
-import pyjokes
-
-listener = sr.Recognizer()
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
 
 
-def talk(text):
-    engine.say(text)
-    engine.runAndWait()
 
 
-def take_command():
+listner = sr.Recognizer()
+
+def speech():
     try:
         with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
+            print("alfred here , how may I help you ")
+            voice = listner.listen(source)
+            command=listner.recognize_google(voice)
+            command=command.lower()
             if 'alfred' in command:
-                command = command.replace('alfred', '')
                 print(command)
+
     except:
         pass
     return command
 
 
-def run_alfred():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
+def talk(t):
+    engine=pt.init()
+    engine.say(t)
+    engine.runAndWait()
+
+
+def run():
+    cd=speech()
+    if 'mouse' in cd:
+        return videoon()
+    elif 'time' in cd:
         time = datetime.datetime.now().strftime('%I:%M %p')
         talk('Current time is ' + time)
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
-    else:
-        talk('Please say the command again.')
+    elif 'eyecam' in cd:
+        return eyecamon()
+
 
 
 while True:
-    run_alfred()
+    run()
+
+
+
+
